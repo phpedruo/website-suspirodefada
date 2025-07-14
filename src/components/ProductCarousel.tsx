e3d8af66e1805c9dc8a69ddcf4074b25caa7122d
phpedruo <phpedruo@gmail.com>
2025-07-13 22:48:11 -0300

    deslizamento carrossel em dispositivos moveis

--- src/components/ProductCarousel.tsx
+++ src/components/ProductCarousel.tsx
@@ -8,16 +8,11 @@ interface ProductCarouselProps {
   onAddToCart: (product: ProductType, quantity: number) => void;
 }
 
-const VISIBLE_COUNT = 3;
-
 const ProductCarousel: React.FC<ProductCarouselProps> = ({ products, onAddToCart }) => {
-  const visibleProducts = products.slice(0, VISIBLE_COUNT);
-
-
   return (
     <div className="carousel-container">
       <div className="carousel-list">
-        {visibleProducts.map(product => (
+        {products.map(product => (
           <ProductCard key={product.id} product={product} onAddToCart={onAddToCart} />
         ))}
       </div>
